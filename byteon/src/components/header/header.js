dbd4d92bcba85b47cf68e4bea5f0ee886ada0548
Shrutik Vankundre <shrutikvankundre1008@gmail.com>
2025-09-30 15:48:06 +0530

    Project Initialization

---
+++ byteon/src/components/header/header.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import '../header/header.css';
+import Logo from '../../assets/images/Logo.png';
+
+
+const Header =()=> {
+    return(
+        <>
+         <header>
+                  <div className='container-fluid'>
+                      <div className='row'>
+                         <div className='col-sm-2' >
+                          <img src={Logo} height={110} alt='Logo'/>
+                        </div>
+
+{/*headerSearch Section*/}
+                        <div className='col-sm-5'>
+                              <div className='headerSearch d-flex align-items-center'>
+                                <div className='seletDrop curso'>
+                                    All Categories
+                                </div>
+                              </div>
+                         </div>
+                     </div>
+                  </div>  
+            </header>
+        </>
+    )
+}
+
+
+export default Header;
